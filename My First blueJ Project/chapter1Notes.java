a498992f6b27ee1036b993cbb7b03993309c07da
Kevin Hayes <khayes@naperville203.org>
2022-08-23 12:38:52 -0500

    Starteed BlueJ and ch1 notes

---
+++ My First blueJ Project/chapter1Notes.java
@@ -0,0 +1,44 @@
+
+/**
+ * Mr. Hayes
+ * 8/23/22
+ * Notes for Ch 1
+ * Write a description where we are going to take some code notes
+ * Who helped me: Mr. Hayes, Mom, Dog, 
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+import java.util.Random;
+
+
+
+public class chapter1Notes
+{
+    /**
+     * main method
+     * where we are going to code
+     */
+    public static void main(String[] args)
+    {
+        
+        Scanner keyboad = new Scanner(System.in);
+        
+        //these are the basic data types
+        int x=9,y,z;
+        double num = 5, num1 = 23.4567, num2;
+        char letter = 'd';
+        boolean choice = false;
+        float anum = 7;
+        long a = 9;
+        short b = 5;
+        
+        
+        
+        //These are objects!
+        String word = new String("Kevin Hayes");
+        Integer number = new Integer(47);
+        
+    }
+}
